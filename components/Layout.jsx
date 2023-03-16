44c392549ff7e878617d6826d830838f87e3d4d1
Jay Vadolkar <jayvadolkar1@gmail.com>
2023-03-17 00:53:26 +0530

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Audiotech: Buy Best Qaulity Headphones Online</title>
       </Head>
       <header>
         <Navbar />
